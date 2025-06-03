cd0efeebb71a0d0d44a69e69a819923ffa5f3921
Yassien Tawfik <Yassien.Mohamed03@eng-st.cu.edu.eg>
2025-06-03 23:43:24 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,60 +1,107 @@
+#include "stm32f4xx.h"
+
+// Core drivers
+#include "Std_Types.h"
 #include "Gpio.h"
 #include "Rcc.h"
-#include "sevenSegment.h"
-#include "Std_Types.h"
-#include "stm32f4xx.h"
 #include "EXTI.h"
+
+// Application drivers
 #include "LCD.h"
+#include "sevenSegment.h"
 #include "ObjectDetection.h"
 
-#define IR_PIN 2
+// Pin definitions
+#define IR_PIN            2  // GPIOA
+#define EMERGENCY_PIN     2  // GPIOC
+#define RESET_PIN         6  // GPIOC
 
+// Global variables
 volatile uint8 Emergency_Flag = 0;
+uint16_t lastObjectCount = 0;
 
-// Emergency stop handler (PC2)
+
+
+//                          INTERRUPT CALLBACKS                              //
+
 void Emergency_Stop_Handler(void) {
     Emergency_Flag = 1;
 }
 
-// Reset button handler (PC6)
 void Reset_Button_Handler(void) {
     NVIC_SystemReset();  // Software reset
 }
 
-int main(void) {
+
+
+//                           INITIALIZATION FUNCTIONS                        //
+
+void Init_Clocks(void) {
     Rcc_Init();
     Rcc_Enable(RCC_GPIOA);
     Rcc_Enable(RCC_GPIOB);
     Rcc_Enable(RCC_GPIOC);
     Rcc_Enable(RCC_SYSCFG);
+}
 
-    ObjectDetection_Init(GPIO_A, IR_PIN);
-
-    LCD_Init();                  // Initialize LCD
+void Init_LCD(void) {
+    LCD_Init();
     LCD_Clear();
-    LCD_SetCursor(0, 0);         // Set cursor to row 0, column 0
+    LCD_SetCursor(0, 0);
+}
 
-    Gpio_Init(GPIO_C, 2, GPIO_INPUT, GPIO_PULL_UP);
-    Gpio_Init(GPIO_C, 6, GPIO_INPUT, GPIO_PULL_UP);
+void Init_ObjectDetection(void) {
+    ObjectDetection_Init(GPIO_A, IR_PIN);
+}
 
-    Exti_Init(PortC, 2, Falling_edge);
-    Exti_Init(PortC, 6, Falling_edge);
+void Init_ButtonsAndInterrupts(void) {
+    // Configure emergency stop and reset buttons
+    Gpio_Init(GPIO_C, EMERGENCY_PIN, GPIO_INPUT, GPIO_PULL_UP);
+    Gpio_Init(GPIO_C, RESET_PIN, GPIO_INPUT, GPIO_PULL_UP);
 
-    Exti_SetCallback(2, Emergency_Stop_Handler);
-    Exti_SetCallback(6, Reset_Button_Handler);
+    // Configure external interrupts
+    Exti_Init(PortC, EMERGENCY_PIN, Falling_edge);
+    Exti_Init(PortC, RESET_PIN, Falling_edge);
 
-    Exti_Enable(2);
-    Exti_Enable(6);
+    Exti_SetCallback(EMERGENCY_PIN, Emergency_Stop_Handler);
+    Exti_SetCallback(RESET_PIN, Reset_Button_Handler);
 
+    Exti_Enable(EMERGENCY_PIN);
+    Exti_Enable(RESET_PIN);
+}
+
+
+
+//                               MAIN PROGRAM                                //
+
+int main(void) {
+    Init_Clocks();
+    Init_LCD();
+    Init_ObjectDetection();
+    Init_ButtonsAndInterrupts();
 
     while (1) {
         __disable_irq();
+
         if (Emergency_Flag) {
             LCD_Clear();
             LCD_SetCursor(0, 0);
             LCD_WriteString(" EMERGENCY STOP");
             Emergency_Flag = 0;
         }
+
         __enable_irq();
+
+        // Update object counter display
+        if (ObjectDetection_Update()) {
+            uint16_t currentCount = ObjectDetection_Get_Count();
+
+            if (currentCount != lastObjectCount) {
+                lastObjectCount = currentCount;
+                LCD_SetCursor(1, 0);
+                LCD_WriteString("Count: ");
+                LCD_PrintNumber(currentCount);
+            }
+        }
     }
 }
