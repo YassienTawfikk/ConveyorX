1b0bcbcda3d0c2e4f552ebce18a23483b875a4e2
Yassien Tawfik <Yassien.Mohamed03@eng-st.cu.edu.eg>
2025-06-05 02:20:48 +0300

    Update ObjectDetection.c

--- ObjectDetection/ObjectDetection.c
+++ ObjectDetection/ObjectDetection.c
@@ -1,46 +1,51 @@
-//
-// Created by Yassien on 6/3/2025.
-//
-
+/* ObjectDetection.c */
 #include "ObjectDetection.h"
 #include "Gpio.h"
+#include "LCD.h"          // ONLY for delay_ms(); remove if you have a separate delay util
 
-static  uint8 sensorPort;
-static  uint8 sensorPin;
-static  uint8 previousStatus = HIGH;
+static uint8  irPort;
+static uint8  irPin;
+static uint16 objectCount = 0;
 
-static  uint16_t objectCount = 0;
-
-void ObjectDetection_Init(uint8 port, uint8 pin) {
-    sensorPort = port;
-    sensorPin = pin;
-
-    Gpio_Init(sensorPort, sensorPin, GPIO_INPUT, GPIO_PULL_UP);
-    previousStatus = Gpio_ReadPin(sensorPort, sensorPin);
+void ObjectDetection_Init(uint8 port, uint8 pin)
+{
+    irPort = port;
+    irPin  = pin;
+    Gpio_Init(irPort, irPin, GPIO_INPUT, GPIO_PULL_UP);
 }
 
-uint8 ObjectDetection_Update(void) {
-    uint8 currentStatus = Gpio_ReadPin(sensorPort, sensorPin);
+/* 
+ * Very-simple algorithm:
+ *   1.  Wait until line goes LOW  ➜ object present / button pressed
+ *   2.  Debounce (20 ms)
+ *   3.  Wait until line returns HIGH ➜ object left / button released
+ *   4.  Count++ and return 1
+ *
+ * It’s blocking for the time the object is in front of the sensor,
+ * but because it is called once per main-loop iteration it works fine
+ * in small projects like this.
+ */
+uint8 ObjectDetection_Task(void)
+{
+    /* Step-1: object? (active-LOW) */
+    if (Gpio_ReadPin(irPort, irPin) == LOW)
+    {
+        /* Step-2: debounce */
+        delay_ms(20);
+        if (Gpio_ReadPin(irPort, irPin) == LOW)
+        {
+            /* Step-3: wait until line released */
+            while (Gpio_ReadPin(irPort, irPin) == LOW);
 
-    if (previousStatus == HIGH && currentStatus == LOW) {
-        delay_milli_second(20); // debounce to avoid multiple counts from bouncing
-        if (Gpio_ReadPin(sensorPort, sensorPin) == LOW) { // confirm still LOW
-            objectCount++;
-            previousStatus = LOW;
-            return 1;
+            /* Step-4: count it */
+            ++objectCount;
+            return 1;          // tell caller “new object detected”
         }
     }
-
-    previousStatus = currentStatus;
-    return 0;
+    return 0;                  // nothing new
 }
 
-uint16_t ObjectDetection_Get_Count(void){
+uint16_t ObjectDetection_GetCount(void)
+{
     return objectCount;
-}
-
-void delay_milli_second(uint32 ms) {
-    uint32 delay = ms * 1000;
-    for (; delay > 0; delay--)
-        __asm("nop");
 }
