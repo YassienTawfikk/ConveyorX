2b01b0b3e6bea40e870f6c81a4b4bdd1ad9927bf
Yassien <yassien.mohamed03@eng-st.cu.edu.eg>
2025-06-05 19:48:43 +0300

    adjust delay methods

--- TimerDelay/TIM_Delay.c
+++ TimerDelay/TIM_Delay.c
@@ -1,35 +1,17 @@
 #include "TIM_Delay.h"
 #include "stm32f4xx.h"
 
-static volatile uint32 system_ms = 0;
 
-void TIM_Delay_Init(void) {
-    // Configure SysTick for 1ms interrupt
-    SysTick_Config(SystemCoreClock / 1000); // 1 ms tick
-
-    // Configure TIM2 for microsecond delay
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Enable TIM2 clock
-    TIM2->PSC = 84 - 1; // Prescaler for 1 MHz (assuming 84 MHz system clock)
-    TIM2->ARR = 0xFFFF; // Max value
-    TIM2->CNT = 0; // Reset counter
-    TIM2->CR1 |= TIM_CR1_CEN; // Enable TIM2
+void delay_ms(uint32 ms)
+{
+    uint32 delay = ms * 668;
+    for (int i = 0; i < delay; i++)  // 668 cycles per ms (rough estimation)
+        __asm__("nop");
 }
 
-void SysTick_Handler(void) {
-    system_ms++;
-}
-
-uint32 millis(void) {
-    return system_ms;
-}
-
-void delay_ms(uint32 ms) {
-    uint32 start = millis();
-    while (millis() - start < ms);
-}
-
-void delay_us(uint32 us) {
-    uint32 start = TIM2->CNT;
-
-    while ((uint16) (TIM2->CNT - start) < us);
+void delay_us(uint32 us)
+{
+    uint32 delay = (us * 0.668) + 1;
+    for (int i = 0; i < delay; i++)
+        __asm__("nop");
 }
--- TimerDelay/TIM_Delay.h
+++ TimerDelay/TIM_Delay.h
@@ -6,10 +6,6 @@
 
 #include "Std_Types.h"
 
-void TIM_Delay_Init(void);
-
-uint32 millis(void);
-
 void delay_ms(uint32 ms);
 
 void delay_us(uint32 us);
