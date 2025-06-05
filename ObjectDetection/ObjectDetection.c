43d18bc79a3c1168d66f1fa5498c214e29a59350
Yassien <yassien.mohamed03@eng-st.cu.edu.eg>
2025-06-05 19:49:18 +0300

    enhance timing

--- ObjectDetection/ObjectDetection.c
+++ ObjectDetection/ObjectDetection.c
@@ -1,34 +1,26 @@
 #include "ObjectDetection.h"
 #include "GPIO.h"
 #include "TIM_Delay.h"
-
 static uint8 od_port;
 static uint8 od_pin;
 
 #define OBJ_PIN od_pin  // for identical naming as your example
 
-void ObjectDetection_Init(uint8 port, uint8 pin) {
+void ObjectDetection_Init(uint8 port, uint8 pin)
+{
     od_port = port;
-    od_pin = pin;
+    od_pin  = pin;
 
     GPIO_Init(od_port, od_pin, GPIO_INPUT, GPIO_PULL_UP);
 }
 
-
 void poll_for_object(uint32 *counter) {
     static uint8 last_pin_state = HIGH;
-    static uint32 last_detection_time = 0;
-
     uint8 current_pin_state = GPIO_ReadPin(od_port, od_pin);
-    uint32 now = millis(); // Current time in ms
 
     if (last_pin_state == HIGH && current_pin_state == LOW) {
-        if (now - last_detection_time >= 150) {
-            // 150ms debounce
-            (*counter)++;
-            last_detection_time = now;
-        }
+        (*counter)++;
+        delay_ms(100);
     }
-
     last_pin_state = current_pin_state;
 }
